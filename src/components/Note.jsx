326768b1c2c4a2483bddac14c62b82d9a43c3ca0
pchen99 <chen_peiting@yahoo.com>
2021-11-24 11:02:34 -0500

    first commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note() {
+   return <div className="note">
+       <h1 >Note title goes here</h1>
+       <p>Note content note content note content</p>
+   </div>
+}
+
+export default Note
